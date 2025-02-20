9aae20b2a8e04cf25894d1a3dfd0192d7297f43b
spitess <133534314+spitess@users.noreply.github.com>
2025-02-20 18:52:29 +0100

    Add files via upload

---
+++ ascii-art-web/asciiart/funcs.go
@@ -0,0 +1,81 @@
+package asciiart
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+var (
+	_map  = make(map[int][8]string)
+	lines = [8]string{}
+)
+
+func InitMap(banner string) bool {
+	var file *os.File
+	var err error
+
+	switch banner {
+
+	case "standard":
+		file, err = os.Open("Banners/standard.txt")
+	case "shadow":
+		file, err = os.Open("Banners/shadow.txt")
+	case "thinkertoy":
+		file, err = os.Open("Banners/thinkertoy.txt")
+	case "bubble":
+		file, err = os.Open("Banners/bubble.txt")
+	case "soft":
+		file, err = os.Open("Banners/soft.txt")
+	default:
+		return false
+	}
+
+	if err != nil {
+		return false
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	// insert data on the map
+	for i := 32; i < 127; i++ {
+		_map[i] = InsertValue(scanner)
+	}
+
+	defer file.Close()
+	return true
+}
+
+func InsertValue(scanner *bufio.Scanner) [8]string {
+	ArtValue := [8]string{}
+
+	for cp := 0; cp < 8 && scanner.Scan(); cp++ {
+		ArtValue[cp] = scanner.Text()
+	}
+	scanner.Scan()
+	return ArtValue
+}
+
+func Storing(inp string) string {
+	res := ""
+
+	if inp == "/n" {
+		res += ""
+	}
+
+	spl := strings.Split(inp, "\n")
+	for _, val := range spl {
+		for _, v := range val {
+			for i := 0; i < 8; i++ {
+				lines[i] += _map[int(v)][i]
+			}
+		}
+		for i := 0; i < 8; i++ {
+			res += lines[i] + "\n"
+			lines[i] = ""
+		}
+	}
+
+	return res
+}
---
+++ ascii-art-web/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	 "html/template"
+	 "log"
+	 "net/http"
+	 "fmt"
+	A "ascii-art-web/asciiart"
+
+)
+
+type Data struct {
+	Result string
+}
+
+func HandleRequest(w http.ResponseWriter, r *http.Request) {  // response 
+	
+	if r.URL.Path != "/"{
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("static/index.html")
+	if err != nil{
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+	
+	if r.Method == http.MethodGet {
+		if err := tmpl.Execute(w, Data{Result: ""}); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		input := r.FormValue("inp1")
+		banner := r.FormValue("Files")
+
+		if !A.InitMap(banner){
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return 
+		}
+
+		if err := tmpl.Execute(w, Data{Result: A.Storing(input)}); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Bad Request", http.StatusBadRequest)	
+}
+
+
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) // this 
+
+	http.HandleFunc("/", HandleRequest)
+	
+	fmt.Println("Server local host 8080")
+	if err:= http.ListenAndServe(":8080", nil) ; err != nil{
+		log.Fatal(err)
+	}
+}
+
